1e95401f7ece4948c961bea3945cb73541b0c7a9
Aldo <aldymc1402@gmail.com>
2024-01-18 18:52:26 -0600

    update3

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:emR$d4pB@localhost:5432/TestDatabase'
+#SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:emR$d4pB@localhost:5432/TestDatabase'
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos_app.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
